867d9263a473924ed51b14239067c0531291054a
Anuj Malviya <75200824+Anuj-malviya0@users.noreply.github.com>
2021-02-26 18:37:01 +0530

    added code

--- Stack/stack.py
+++ Stack/stack.py
@@ -1,1 +1,23 @@
-
+#creating stack 
+class stack():
+    '''This objects uses list to implement stack '''
+    #creating constructor
+    def __init__(self):        
+        self.items = []
+        self.top = 0
+    #creating push method 
+    def push(self,value):
+        '''inserts a element in to top of stack'''
+        self.items.append(value)
+        self.top = self.items[-1]
+    #creating pop method
+    def pop(self):
+        '''deletes a element from top of stack'''
+        self.items.pop()
+        '''if stack is empty then top will have null value'''
+        if self.items:
+            self.top = self.items[-1]
+        else:
+            self.top = None
+        
+    
