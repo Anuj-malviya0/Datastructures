27db37f1e49d00b1b9b24052f1841c214ba91d90
Anuj Malviya <75200824+Anuj-malviya0@users.noreply.github.com>
2021-03-05 21:20:59 +0530

    added main code

--- Queue/queue.py
+++ Queue/queue.py
@@ -1,1 +1,30 @@
-#contents will be added soon 
+class Queue():
+    #creating constructor
+    def __init__(self,*args):
+        self.items = [*args]
+        # A list will contain values of queue 
+        if self.items:
+            self.front = self.items[0]
+            self.rear = self.items[-1]
+        else:
+            self.front = None
+            self.rear = None
+    #enque method will add an element to the queue
+    def enque(self,value):
+        self.items.insert(0,value)
+    #here we are setting rear and front of queue
+        self.front = self.items[0]
+        self.rear = self.items[-1]
+    #dequeue mthod which will delete items from the rear of the queue 
+    def dequeue(self):
+        self.items.pop()
+        #here we are adjusting the rear 
+        if self.items:
+            self.rear = self.items[-1]
+        else:
+            self.front = None
+            self.rear = None
+    #here we are creating the string representaion of our queue
+    def __str__(self):
+        return f'Queue{self.items}'
+            
